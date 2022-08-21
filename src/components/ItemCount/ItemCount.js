676028d300c02336c01b81df180f23a22f4ee743
Federico Vacca <fede.mxpx@gmail.com>
2022-08-21 14:46:04 -0300

    Cuarta entrega Contador con boton

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import "./ItemCount.css"
+
+const ItemCount = ({ stock = 5, initial = 1, onAdd }) => {
+    const [quantity, setQuantity] = useState(initial)
+
+    const minus = () => {
+        if ( quantity > 0) {
+            setQuantity(quantity - 1)
+        }
+    }
+
+    const plus = () => {
+        if ( quantity < stock) {
+            setQuantity(quantity + 1)
+        }
+    }
+
+    return(
+        <div className="container">
+            <div className="count count_container">
+                <div className="contar">
+                    <button onClick={minus} className="btn-stock"> - </button>
+                    <h5 className="elementos">{quantity}</h5>
+                    <button onClick={plus} className="btn-stock"> + </button>
+                </div>
+            </div>
+            <div>
+                <button onClick={() => onAdd(quantity)} className="btn-agregar">Agregar al carrito</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,12 @@
 import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) => {
     return (
         <div>
+            
             <h1 className="greeting">{props.greeting}</h1>
+            <ItemCount/>
         </div>
     )
 };
